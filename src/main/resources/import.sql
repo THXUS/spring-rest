47312f0a99ec70cb00f1d3926f1f6b5c2c430b77
Matheus Santana <matheus11_santana@hotmail.com>
2022-04-16 20:42:37 -0300

    Adição de dados para cidade e estado

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,7 @@
 insert into cozinha (cz_nome) values ('Tailandesa'), ('Coreana');
 
 insert into restaurante(res_nome, res_taxafrete, res_cozinha) values ('Shabrang', 10,1), ('Tunak Tunak', 25.15,1);
+
+insert into estado(es_nome) values ('Goiás'), ('Rio de Janeiro');
+
+insert into cidade(cd_nome, cd_estado) values ('Senador Canedo', 1), ('Goiânia', 1);
