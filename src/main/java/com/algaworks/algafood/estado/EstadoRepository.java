f4df81f255a79a12416cceb70df526e4f1465d38
Matheus Santana <matheus11_santana@hotmail.com>
2022-04-17 15:10:29 -0300

    Criação de métodos de Delete

--- src/main/java/com/algaworks/algafood/estado/EstadoRepository.java
+++ src/main/java/com/algaworks/algafood/estado/EstadoRepository.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.domain.entity.Estado;
@@ -17,6 +19,25 @@ public class EstadoRepository {
     
     public List<Estado> listar() {
         return this.entityManager.createQuery("from Estado", Estado.class).getResultList();
+    }
+    
+    @Transactional
+    public void excluir(final Estado entidade) {
+        
+        this.entityManager.remove(this.entityManager.contains(entidade) ? entidade : this.entityManager.merge(entidade));
+        
+    }
+    
+    public Estado getPorCodigo(final Long codigo) {
+        final Estado entidade = this.entityManager.find(Estado.class, codigo);
+        
+        if (entidade != null) {
+            this.entityManager.detach(entidade);
+            return entidade;
+        }
+        
+        throw new EmptyResultDataAccessException(1);
+        
     }
     
 }
