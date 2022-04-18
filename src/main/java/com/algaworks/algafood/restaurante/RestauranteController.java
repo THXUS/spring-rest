c73af06e8ad2ddfdd343e8c9985c8b28b5074884
Matheus Santana <matheus11_santana@hotmail.com>
2022-04-17 22:22:14 -0300

    Adição de método put

--- src/main/java/com/algaworks/algafood/restaurante/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/restaurante/RestauranteController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.domain.entity.Restaurante;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontrada;
+import com.algaworks.algafood.domain.exception.SingletonNaoEncontrado;
 
 @RestController
 @RequestMapping("/restaurante")
@@ -32,7 +34,7 @@ public class RestauranteController {
     public ResponseEntity<?> getPorCodigo(@PathVariable("codigo") final Long codigo) {
         try {
             return ResponseEntity.ok(this.restauranteService.getPorCodigo(codigo));
-        } catch (final EntidadeNaoEncontrada e) {
+        } catch (final SingletonNaoEncontrado e) {
             return ResponseEntity.notFound().build();
         }
     }
@@ -48,6 +50,17 @@ public class RestauranteController {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
         
+    }
+    
+    @PutMapping("/{codigo}")
+    public ResponseEntity<?> alterar(@PathVariable("codigo") final Long codigo, @RequestBody final Restaurante restaurante) {
+        try {
+            return ResponseEntity.ok(this.restauranteService.alterar(codigo, restaurante));
+        } catch (final EntidadeNaoEncontrada e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        } catch (final SingletonNaoEncontrado e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
     
 }
